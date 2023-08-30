ade09c9f2712e7290d33ad4846cb9520787c50c3
miyamone <monmon38mon@gmail.com>
2023-08-30 13:00:40 +0900

    IQAE final roundでのbiasを理論近似式と比較　→　テイラー展開破綻する近傍で乖離大

--- IQAEBinom_OnlyFinalRound.py
+++ IQAEBinom_OnlyFinalRound.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def IQAEBinom_OnlyFinalRound(ampSq, nGrover, nShot):
+def IQAEBinom_OnlyFinalRound(ampSq, nGrover, nShot, nEstim=None):
 
     theta = np.arcsin(np.sqrt(ampSq))
     kRound = 2 * nGrover + 1
     prob1 = np.sin(kRound * theta) ** 2
     rRound = int(kRound * theta / (0.5 * np.pi))
 
-    p1Obs = np.random.binomial(nShot, prob1) / nShot
+    p1Obs = np.random.binomial(nShot, prob1, size=nEstim) / nShot
 
     if rRound % 2 == 0:
         gammaML = np.arcsin(np.sqrt(p1Obs))
